3507c678d9d3be1df47197b7fad8417f0e4e013a
Sean <1430109768@qq.com>
2022-01-16 23:46:31 +0800

    feat: add demo1

---
+++ demo1/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('hello world'));
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
